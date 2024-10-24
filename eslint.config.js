4ce588284719210758c73c965a3e74d230d6f8d1
ayeshaArif6 <[aarif6@asu.edu]>
2024-10-24 15:43:51 -0400

    installing more dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import pkg from '@eslint/js';
 import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import globals from 'globals';
 
 const { configs: eslintRecommended } = pkg;
@@ -21,6 +23,8 @@ export default [
         },
         plugins: {
             react: reactRecommended, // Add React plugin to recognize JSX
+            'react-hooks': reactHooks,
+            'jsx-a11y': jsxA11y,
         },
         ...eslintRecommended.recommended,
         ...reactRecommended,
@@ -33,6 +37,8 @@ export default [
             ],
             'react/react-in-jsx-scope': 'off', // Disable the need for React to be in scope
             'react/jsx-uses-vars': 'error', // Ensure variables used in JSX are marked as used
+            'react-hooks/exhaustive-deps': 'warn', // React hooks exhaustive dependencies rule
+            'jsx-a11y/anchor-is-valid': 'warn', // Accessibility rule for anchor elements
         },
     },
 ];
